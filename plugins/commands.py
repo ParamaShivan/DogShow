68035f8496799bf0f6b1c0b235def9f9ae9f2f44
Son Of Xander <shalualexandetlukose@gmail.com>
2022-06-21 22:18:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,7 +45,7 @@ async def start(client, message):
             InlineKeyboardButton('🔍 Search Inline', switch_inline_query_current_chat='')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(
+        await message.reply_text(
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
@@ -84,7 +84,7 @@ async def start(client, message):
             InlineKeyboardButton('🔍 Search Inline', switch_inline_query_current_chat='')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(
+        await message.reply_text(
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'

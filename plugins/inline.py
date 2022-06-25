269c78b0a646570c82bdf0159e4b2eeb11c78ab8
Son Of Xander <shalualexandetlukose@gmail.com>
2022-06-25 21:49:30 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -27,7 +27,7 @@ async def answer(bot, query):
         await query.answer(results=[],
                            cache_time=0,
                            switch_pm_text='ബ്രോ, മറ്റുള്ളവർ റിക്വസ്റ്റ് ചെയിത മൂവിയിൽ കുത്തി നോക്കാതെ ബ്രോന് വേണ്ടത് ബ്രോ റിക്വസ്റ്റ് ചെയ്യുക.\n\nBro. Search Your Own File, Dont Click Others Requested Files.',
-                           switch_pm_parameter='its not for you.'
+                           switch_pm_parameter='its not for you.')
              return
 
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
